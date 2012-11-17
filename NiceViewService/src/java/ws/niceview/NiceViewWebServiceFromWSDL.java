d3166dcbabc467d780d04700aeccb9a46205a285
Wojciech Pawlak <wojciechpawlak@gmail.com>
2012-11-17 11:45:58 +0100

    Fixed WSDL for NiceView to make BPEL tests work.
    Added namespaces to WSDL file for NiceView.

--- NiceViewService/src/java/ws/niceview/NiceViewWebServiceFromWSDL.java
+++ NiceViewService/src/java/ws/niceview/NiceViewWebServiceFromWSDL.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ws.niceview;
 
 import dk.dtu.imm.fastmoney.types.CreditCardFaultType;
@@ -22,8 +17,7 @@ import ws.niceview.types.HotelType;
 public class NiceViewWebServiceFromWSDL {
 
     private int DAY_LIMIT = 14;
-
-    private HotelType[] hotelArray = new HotelType[] {
+    private HotelType[] hotelArray = new HotelType[]{
         createHotel("Superb Hotel", "TestStreet", "12345", "Barcelona", "1", 50000.0, true),
         createHotel("Nice Hotel", "TestStreet", "12345", "Vienna", "2", 2000.0, true),
         createHotel("Passable Hotel", "TestStreet", "12345", "Vienna", "3", 500.0, true),
@@ -37,7 +31,7 @@ public class NiceViewWebServiceFromWSDL {
         Random rand = null;
 
         if (arrivalDate.get(java.util.Calendar.DAY_OF_YEAR) -
-               departureDate.get(java.util.Calendar.DAY_OF_YEAR) > DAY_LIMIT) {
+                departureDate.get(java.util.Calendar.DAY_OF_YEAR) > DAY_LIMIT) {
             randomize = true;
             rand = new Random(System.currentTimeMillis());
         }
@@ -52,7 +46,7 @@ public class NiceViewWebServiceFromWSDL {
 
                 } else {
                     hotelList.getHotel().add(hotel);
-                    
+
                 }
             }
         }
@@ -62,17 +56,20 @@ public class NiceViewWebServiceFromWSDL {
     }
 
     public boolean bookHotel(java.lang.String bookingNumber, dk.dtu.imm.fastmoney.types.CreditCardInfoType creditCardInfo) throws BookHotelFault, BookHotelCreditCardFault {
-        if ("".equals(bookingNumber))
+        if ("".equals(bookingNumber)) {
             throw new BookHotelFault("Booking number cannot be empty", new BookHotelFaultType());
+        }
 
-        if ("".equals(creditCardInfo.getNumber()))
+        if ("".equals(creditCardInfo.getNumber() )) {
             throw new BookHotelCreditCardFault("Number cannot be empty", new CreditCardFaultType());
+        }
         return true;
     }
 
     public boolean cancelHotel(java.lang.String bookingNumber) throws CancelHotelFault {
-        if ("".equals(bookingNumber))
+        if ("".equals(bookingNumber)) {
             throw new CancelHotelFault("Booking number cannot be empty", new CancelHotelFaultType());
+        }
         return true;
     }
 
@@ -87,12 +84,11 @@ public class NiceViewWebServiceFromWSDL {
         hotelAddress.setPostcode(hotelPostcode);
         hotelAddress.setCity(hotelCity);
         hotel.setHotelAddress(hotelAddress);
-        
+
         hotel.setBookingNumber(bookingNumber);
         hotel.setHotelStayPrice(stayPrice);
         hotel.setIfCreditCardRequired(ifCreditCardGuaranteeRequired);
 
         return hotel;
     }
-
 }
